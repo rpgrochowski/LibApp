4e41e4bb5df81eed96657932a9b281d883654d25
Rafał Grochowski <wiliam1arafal@gmail.com>
2021-10-17 12:21:11 +0200

    Added...

---
+++ Controllers/BooksControler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibApp2.Models;
+
+namespace LibApp2.Controllers
+{
+    public class BooksControler : Controller
+    {
+        public IActionResult Index()
+        {
+            var firstBook = new Book() { Name = "English dictionary" };
+
+            return View(firstBook);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibApp2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
